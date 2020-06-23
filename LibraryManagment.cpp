27e119c1fd757da973ea35b563d80087b1de9da0
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-23 13:43:41 +0200

    Uploading main source file

---
+++ LibraryManagment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Book.h"
+
+int main()
+{
+    Book TestBook;
+    Book TestBookTwo("Harry Potter", 700);
+    std::cout << TestBook.getName() << std::endl;
+    std::cout << TestBookTwo.getName() << std::endl;
+}
+
