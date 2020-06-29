a5d35a7b7a98681584fedd707490d9653dcfb1c4
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-29 15:06:48 +0200

    Update Book.cpp

--- ResourceFiles/Book.cpp
+++ ResourceFiles/Book.cpp
@@ -1,7 +1,41 @@
 #include "Book.h"
 
-Book::Book() : Name("Error"), Pages(0) {}
-Book::Book(std::string nName, int nPages) : Name(nName), Pages(nPages) {}
-std::string Book::getName() {
-	return Name;
+Book::Book() : Author("Error"), Title("Error"), Pages(0), Genre("None"), Quantity(0) {}
+Book::Book(std::string nAuthor, std::string nTitle,
+	int nPages , std::string nGenre, 
+	int nQuantity) :
+	Author(nAuthor),
+	Title(nTitle), 
+	Pages(nPages),
+	Genre(nGenre),
+	Quantity(nQuantity){}
+std::string Book::getAuthor() {
+	return this->Author;
+}
+void Book::setAuthor(std::string newAuthor) {
+	this->Author = newAuthor;
+}
+std::string Book::getTitle() {
+	return this->Title;
+}
+void Book::setTitle(std::string newTitle) {
+	this->Title = newTitle;
+}
+int Book::getPages() {
+	return this->Pages;
+}
+void Book::setPages(int newPages) {
+	this->Pages = newPages;
+}
+std::string Book::getGenre() {
+	return this->Genre;
+}
+void Book::setGenre(std::string newGenre) {
+	this->Genre = newGenre;
+}
+int Book::getQuantity() {
+	return this->Quantity;
+}
+void Book::setQuantity(int nQuantity) {
+	this->Quantity = nQuantity;
 }
