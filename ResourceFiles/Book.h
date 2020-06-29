e9a1d690145ed6b9bd69e1ef61c4113b5345d126
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-29 15:07:09 +0200

    Update Book.h

--- ResourceFiles/Book.h
+++ ResourceFiles/Book.h
@@ -1,10 +1,24 @@
 #pragma once
 #include <string>
 class Book {
-	std::string Name;
+	std::string Author;
+	std::string Title;
 	int Pages;
+	std::string Genre;
+	int Quantity;
 public:
 	Book();
-	Book(std::string nName, int nPages);
-	std::string getName();
+	Book(std::string nAuthor, std::string nTitle, int nPages, 
+		std::string nGenre, int nQuantity);
+	std::string getAuthor();
+	void setAuthor(std::string newAuthor);
+	std::string getTitle();
+	void setTitle(std::string newTitle);
+	int getPages();
+	void setPages(int newPages);
+	std::string getGenre();
+	void setGenre(std::string newGenre);
+	int getQuantity();
+	void setQuantity(int nQuantity);
+
 };
