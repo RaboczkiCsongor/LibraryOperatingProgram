72e4f29987238ed4a4782fa99258f908c4f65437
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-29 13:05:18 +0200

    Update LibraryManagment.cpp

--- LibraryManagment.cpp
+++ LibraryManagment.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "Book.h"
+#include "MemClear.h"
+#include "Menu.h"
+#include "vld.h"
+
 
 int main()
 {
-    Book TestBook;
-    Book TestBookTwo("Harry Potter", 700);
-    std::cout << TestBook.getName() << std::endl;
-    std::cout << TestBookTwo.getName() << std::endl;
+    std::vector<Book*> Books;
+    
+    Books.push_back(new Book());
+    Books.push_back(new Book("J.K. Rowling","Harry Potter", 700 , "Fantasy",10));
+
+    displayMenu(Books);
+
+
+    //std::cout << Books[0]->getName() << std::endl;
+    //std::cout << Books[1]->getName() << std::endl;
+
+    ClearMemory(Books);
     return 0;
 }
+
