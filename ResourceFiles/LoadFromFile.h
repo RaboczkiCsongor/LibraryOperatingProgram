e52c69a4c947d7f2a7d52b31ac5437fa1eb2de69
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-29 15:09:26 +0200

    Create LoadFromFile.h

---
+++ ResourceFiles/LoadFromFile.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Book.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+void LoadFromFile(std::vector<Book*> &myVector);
