15de497c2b8710716de63751403317f00be51797
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-29 15:08:04 +0200

    Update Menu.h

--- ResourceFiles/Menu.h
+++ ResourceFiles/Menu.h
@@ -4,9 +4,11 @@
 #include <sstream>
 #include <vector>
 #include "Book.h"
-
+#include "SaveToFile.h"
+#include "LoadFromFile.h"
 constexpr auto MenuWidth = 15;
 
+void SearchForBookByName(std::string name, std::vector<Book*> &myVector);
 void line();
 void displayMenu(std::vector<Book*> &myVector);
 void MenuOptions();
