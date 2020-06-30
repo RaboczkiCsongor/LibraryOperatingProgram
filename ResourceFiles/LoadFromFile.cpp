c544b723d009c7705de05f3e548d1cae9b3863ab
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-06-30 10:36:17 +0200

    Fixing LoadingFromFile

--- ResourceFiles/LoadFromFile.cpp
+++ ResourceFiles/LoadFromFile.cpp
@@ -4,38 +4,39 @@
 void LoadFromFile(std::vector<Book*> &myVector) {
 	std::ifstream myFile;
 	std::string line;
-	std::string Author ="", Title = "", Genre = "";
-	int Pages = 0, Quantity = 0;
-	int SavePos = 0;
 	myFile.open("BookList.txt");
 	if (myFile.is_open()) {
 		while (!myFile.eof()) {
 			std::getline(myFile, line);
+			std::string Author = "", Title = "", Genre = "";
+			int Pages = 0, Quantity = 0;
+			int SavePos = 0;
 			for (int i = 0; i < line.length(); i++) {
+
 				if (line[i] == '\t' && Author == "") {
 					Author = line.substr(0, i);
 					SavePos = i;
 					continue;
 				}
 				if (line[i] == '\t' && Title == "") {
-					Title = line.substr(SavePos+1, i);
+					Title = line.substr(SavePos+1, i-SavePos-1);
 					SavePos = i;
 					continue;
 				}
 				if (line[i] == '\t' && Pages == 0) {
-					std::string s = line.substr(SavePos+1, i);
+					std::string s = line.substr(SavePos+1, i - SavePos);
 					std::stringstream converted(s);
 					converted >> Pages;
 					SavePos = i;
 					continue;
 				}
 				if (line[i] == '\t' && Genre == "") {
-					Genre = line.substr(SavePos + 1, i);
+					Genre = line.substr(SavePos + 1, i - SavePos-1);
 					SavePos = i;
 					continue;
 				}
-				if (line[i] == '\0' && Quantity == 0) {
-					std::string s = line.substr(SavePos+1, i);
+				if (Quantity == 0 && Genre != "") {
+					std::string s = line.substr(SavePos+1, line.length());
 					std::stringstream converted(s);
 					converted >> Quantity;
 				}
