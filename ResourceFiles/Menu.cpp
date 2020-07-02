89746c4181d392d838e9ed7393abbe03a9a5de86
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-02 12:19:28 +0200

    Update Menu.cpp

--- ResourceFiles/Menu.cpp
+++ ResourceFiles/Menu.cpp
@@ -5,27 +5,25 @@ void line() {
 	std::cout <<std::endl;
 }
 void MenuOptions() {
-	std::cout << "6. Load from file" << std::endl;
-	std::cout << "5. Save to file" << std::endl;
+	std::cout << "5. Save/Load" << std::endl;
 	std::cout << "4. Remove a book" << std::endl;
-	std::cout << "3. Add a book" << std::endl;
+	std::cout << "3. Add a new book" << std::endl;
 	std::cout << "2. Search" << std::endl;
 	std::cout << "1. List all books"<< std::endl;
 	std::cout << "0. Exit" << std::endl;
 }
 int InputCheck() {
-	std::string Selection = "";
-	int SelectionNum;
+	std::string InputString = "";
+	int OutputInt;
 	std::cout << "Please select: ";
 	while (true) {
-		std::getline(std::cin, Selection);
-		std::stringstream myStream(Selection);
-		if ((myStream >> SelectionNum))
+		std::getline(std::cin, InputString);
+		std::stringstream myStream(InputString);
+		if ((myStream >> OutputInt))
 			break;
 		std::cout << "Invalid input, please try again" << std::endl;
 	}
-	
-	return SelectionNum;
+	return OutputInt;
 }
 void SearchForBookByName(std::string name, std::vector<Book*>& myVector) {
 	std::string SearchTerm;
@@ -39,36 +37,49 @@ void SearchForBookByName(std::string name, std::vector<Book*>& myVector) {
 		}
 	}
 }
+void ListAllMenu(std::vector<Book*>& myVector) {
+	int idx = 1;
+	for (auto p : myVector) {
+		std::cout << idx << ". ";
+		p->PrintForList();
+		idx++;
+	}
+}
+
+void RemoveBook(std::vector<Book*>& myVector) {
+	int removeidx;
+	ListAllMenu(myVector);
+	std::cout << "Please provide the ID number of book you would like to remove from inventory" << std::endl;
+	removeidx = InputCheck() -1;
+	delete myVector[removeidx];
+	//myVector.erase(myVector.begin() + removeidx);
+}
 
 void displayMenu(std::vector<Book*> &myVector) {
 	int Selection;
 		do{
-			int idx = 1;
 			line();
 			MenuOptions();
 			line();
 			Selection = InputCheck();
 			switch (Selection) {
 			case 1: // list all
-				for (auto p : myVector) {
-					std::cout << idx << ". ";
-					std::cout << p->getAuthor() << "\t";
-					std::cout << p->getTitle() << "\t";
-					std::cout << "Available: "<<p->getQuantity() << std::endl;
-					idx++;
-				}
+				ListAllMenu(myVector);
 				break;
 			case 2: // search for book
 				break;
 			case 3: // add book
+				AddBook(myVector);
 				break;
 			case 4: //remove book
+				RemoveBook(myVector);
 				break;
-			case 5: 
-				SaveToFile(myVector);
-				break;
-			case 6:
-				LoadFromFile(myVector);
+			case 5:
+				line();
+				DisplaySaveLoadMenu();
+				line();
+				int LoadSaveSelection = InputCheck();
+				SelectionSaveLoadMenu(myVector, LoadSaveSelection);
 				break;
 			}
 		} while (Selection != 0);
