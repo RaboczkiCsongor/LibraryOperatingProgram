e067916c1c91ef9e1af83943e628bba26017a520
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-02 12:18:20 +0200

    Create AddBook.h

---
+++ ResourceFiles/AddBook.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Book.h"
+
+int InputCheckForAddBook();
+void AddBook(std::vector<Book*>& myVector);
+
