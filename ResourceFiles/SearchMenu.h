5f196d807cecfcde6b8fa4a03253bfd09271418f
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-04 11:26:49 +0200

    Create SearchMenu.h

---
+++ ResourceFiles/SearchMenu.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Book.h"
+#include"InputHeader.h"
+#include "SearchFunction.h"
+#include "line.h"
+
+
+void DisplaySearchMenu(std::vector<Book*> &myVector);
