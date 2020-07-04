8187dbb57a293b5ad30133303cfeb1ff5918243a
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-04 11:26:31 +0200

    Create SearchFunction.h

---
+++ ResourceFiles/SearchFunction.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Book.h"
+#include "line.h"
+
+void SearchForBooks(std::vector<Book*>& myVector, int Identifier);
+void ListAllMenu(std::vector<Book*>& myVector);
