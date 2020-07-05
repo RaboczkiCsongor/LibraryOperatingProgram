0217b042854596bceef73e48ab887c5489100d75
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-05 11:01:12 +0200

    Create Restoration.h

---
+++ ResourceFiles/Restoration.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <thread>
+#include "Book.h"
+#include "SearchFunction.h"
+#include "InputHeader.h"
+
+void Restoration(std::vector <Book*>& myVector);
