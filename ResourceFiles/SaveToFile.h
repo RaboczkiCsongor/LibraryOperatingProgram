06b2a80bf89ffccd80299f85ccae3863f095cbec
RaboczkiCsongor <54138095+RaboczkiCsongor@users.noreply.github.com>
2020-07-05 11:02:08 +0200

    Update SaveToFile.h

--- ResourceFiles/SaveToFile.h
+++ ResourceFiles/SaveToFile.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Book.h"
 #include <fstream>
+#include <string>
 #include <vector>
 
-void SaveToFile(std::vector<Book*> myVector);
+void SaveToFile(std::vector<Book*> myVector,std::string FileName);
